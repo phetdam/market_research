9e7d3a6d6e1fd84447f5939e897d3f9559c9370a
Derek <djh458@stern.nyu.edu>
2018-10-24 20:01:40 -0400

    first foray into interest rate models; may be doing something later with that treasury data but idk rn

---
+++ cir.py
@@ -0,0 +1,55 @@
+# basic implementation of a cox-ingersol-ross single-factor short rate model
+# https://en.wikipedia.org/wiki/Cox-Ingersoll-Ross_model
+#
+# implementation of model:
+#
+# dr = a * (mu - r) * dt + sigma * math.sqrt(r) * np.random.normal() * math.sqrt(dt)
+# we assume that dW is driven by z * sqrt(dt), where z ~ N(0, 1)
+#
+# Changelog:
+#
+# 10-24-2018
+#
+# split off from cir_main.py (the original cir.py) into standalone file
+
+# import math, pandas, numpy (assume latter two exist)
+import math
+import pandas as pd
+import numpy as np
+
+# program name
+PROGNAME = "cir"
+
+# cir generating function
+# driven by sigma, with a to govern speed of mean reversion, while mu establishes the mean
+# dt is the differential timestep, and n is the number of times to loop
+# by default, returns dataframe, but if df = False, will return tuple of ndarrays (x, y)
+# unless specified otherwise, will always assume the cir process it returns is the first
+# one being created (can specify number with cc = x)
+def return_cir(a, mu, dt, sigma, n, cc = 0, df = True):
+    # starting point of r assumed to be mu
+    r = mu
+    # differential change of r dr starts as 0
+    dr = 0
+    # np array for the x axis (time)
+    x = np.linspace(0, n - 1, n)
+    # np array for the y axis (r)
+    y = np.empty(n)
+    # for n iterations
+    for i in range(n):
+        # insert r as value in np array
+        y[i] = r
+        # calculate dr
+        dr = a * (mu - r) * dt + sigma * math.sqrt(r) * np.random.normal() * math.sqrt(dt)
+        # add change of dr to r
+        r += dr
+    # if df is True, wrap in dataframe and return (label as cir_(cc))
+    if (df == True):
+        return pd.DataFrame(index = x, data = y, columns = ["cir_{}".format(cc)])
+    # else return ndarrays as a tuple
+    return (x, y)
+
+# main
+if (__name__ == "__main__"):
+    print("{}: do not run in standalone mode.".format(PROGNAME))
+    quit(1)
---
+++ cir_main.py
@@ -0,0 +1,91 @@
+# contains basic implementation of a cox-ingersol-ross single-factor short rate model
+# https://en.wikipedia.org/wiki/Cox-Ingersoll-Ross_model
+#
+# implementation of model:
+#
+# dr = a * (mu - r) * dt + sigma * math.sqrt(r) * np.random.normal() * math.sqrt(dt)
+# we assume that dW is driven by z * sqrt(dt), where z ~ N(0, 1)
+#
+# Changelog:
+#
+# 10-24-2018
+#
+# initial creation; git commit. renamed to cir_main.py
+
+# program name
+PROGNAME = "cir"
+
+# help flag
+HELP_FLAG = "--help"
+
+# help string
+HELP_STR = ("Usage: {0}\n       {0} {1}\n"
+            "simulates a single run of a CIR process. more to come later!".format(PROGNAME, HELP_FLAG)
+    )
+
+# default file to save to
+DEFAULT_F = "cir.png"
+
+# indicates how many cir processes have been generated
+CIR_COUNT = 0
+
+# configurations for cir; list [a, mu, dt, sigma, n]
+CIR_PARAM = [0.03, 0.1, 0.001, 0.07, 1000]
+
+# import matplotlib; catch exception
+try:
+    import matplotlib.pyplot as plt
+except:
+    print("{}: please install matplotlib.".format(PROGNAME))
+    quit()
+# import math lib and sys
+import math
+import sys
+# import pandas and numpy
+import pandas as pd
+import numpy as np
+# import cir
+import cir
+
+# main
+if (__name__ == "__main__"):
+    # get length of arguments in the argv vector
+    argc = len(sys.argv)
+    # if there is only one argument, run with some default values
+    if (argc == 1):
+        # figure size width 6', height 4.5'
+        fg = plt.figure(figsize = (12, 9))
+        # get x (t) and y (r) series of cir process; element 0 is time series, 1 is r
+        x1, y1 = cir.return_cir(*CIR_PARAM, cc = CIR_COUNT, df = False)
+        # plot the two series (x1, y1 and x2, y2)
+        # configurations for cir; list [a, mu, dt, sigma, n]
+        plt.plot(x1, y1, label = "cir_{0}(a={1}, mu={2}, dt={3}, sigma={4}, n={5})".format(
+            CIR_COUNT, *CIR_PARAM))
+        # increment CIR_COUNT
+        CIR_COUNT += 1
+        # get x (t) and y (r) series of cir process; element 0 is time series, 1 is r
+        # we do this second one just for comparison reasons; plot
+        x2, y2 = cir.return_cir(*CIR_PARAM, cc = CIR_COUNT, df = False)
+        plt.plot(x2, y2, label = "cir_{0}(a={1}, mu={2}, dt={3}, sigma={4}, n={5})".format(
+            CIR_COUNT, *CIR_PARAM))
+        # increment CIR_COUNT
+        CIR_COUNT += 1
+        # x label, y label (mmake vertical)
+        plt.xlabel("t")
+        plt.ylabel("r", rotation = 0)
+        # graph title
+        plt.title("CIR process")
+        # show legend
+        plt.legend()
+        # save to file
+        plt.savefig(DEFAULT_F)
+    # else if there are two arguments
+    elif (argc == 2):
+        # if it is the help option
+        if (sys.argv[1] == HELP_FLAG):
+            print(HELP_STR)
+            quit()
+    # else too many arguments
+    else:
+        print("{0}: too many arguments. type '{0} {1}' for usage.".format(PROGNAME, HELP_FLAG))
+        quit()
